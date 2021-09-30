1a3617f413f7fde6b5001b1deb6ef05164b8d34a
Juan Manuel Varela <jmvarela27@gmail.com>
2021-09-30 14:39:24 -0300

    admin home y forms

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -17,16 +17,33 @@ async function index(req, res) {
 async function show(req, res) {}
 
 // Show the form for creating a new resource
-async function create(req, res) {}
+async function create(req, res) {
+  res.render("adminCrear");
+}
 
 // Store a newly created resource in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  res.send("entré al crear");
+}
 
 // Show the form for editing the specified resource.
-async function edit(req, res) {}
+async function edit(req, res) {
+  const id = req.params.id;
+  const articulo = await Article.findByPk(id, {
+    include: [
+      {
+        model: User,
+        required: true,
+      },
+    ],
+  });
+  res.render("adminEditar", { articulo });
+}
 
 // Update the specified resource in storage.
-async function update(req, res) {}
+async function update(req, res) {
+  res.send("entré al update");
+}
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {}
---
+++ public/js/admin.js
@@ -0,0 +1,19 @@
+document.addEventListener("click", (e) => {
+  // Cerrar modal
+  if (e.target.matches("#cancelar-modal") || e.target.matches("#dark-background")) {
+    document.getElementById("dark-background").classList.remove("show");
+    document.getElementById("modal-box").classList.remove("show");
+    document.querySelector("#contenido-sin-modal").classList.remove("blur-background");
+  }
+});
+
+function clickEliminar(id) {
+  document.getElementById(
+    "modal-msg",
+  ).textContent = `Seguro quiere eliminar el usuario con ID ${id}`;
+  document.getElementById("aceptar-modal").href = `/usuarios/eliminar/${id}`;
+
+  document.getElementById("dark-background").classList.add("show");
+  document.getElementById("modal-box").classList.add("show");
+  document.querySelector("#contenido-sin-modal").classList.add("blur-background");
+}
